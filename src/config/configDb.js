2d83f0d65df30d24d940cff401a33026e9460f33
Bairon Sanhueza <bairon.sanhueza2001@alumnos.biobio.cl>
2024-10-28 00:59:23 -0400

    Modificacion de archivos

---
+++ src/config/configDb.js
@@ -0,0 +1,24 @@
+"user strict";
+import { DataSource } from "typeorm";
+import {DATABASE, DB_USERNAME, HOST, PASSWORD} from './configENV.js';
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: '${HOST}',
+    port: 5432,
+    username: '${DB_USERNAME}',
+    password: '${PASSWORD}',
+    database: '${DATABASE}',
+    entities: ["src/entity/**/*.js"],
+    synchronize: true,
+    logging: false,
+});
+
+export async function connectDB(){
+    try{
+        await AppDataSource.initialize();
+        console.log("=> conexion a la basa de datos exitosa!");
+    }catch (error){
+        console.error("error al conectarse: ", error);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-import express from 'express'; 
+import express, { json } from 'express'; 
+import indexRoutes from './routes/index.routes.js';
+import {PORT, HOST} from './config/configENV.js';
+import { connectDB} from './config/configDb.js';
+
+async function setupServer() {
+    try{
+        const app = express();
+
+        app.use(json());
+        app.use('/api', idexRoutes);
+        app.listen(PORT, () =>{
+            console.log `servidor corriendo en: http://${HOST}:${PORT}/api`;
+        } )
+    }catch (error){
+        console.error("error eb index.js -> setupServer(), el error es: ", error);
+    }
+}
+
+async function setupAPI() {
+    try{
+        await connectDB();
+        await setupServer();
+    }catch(error){
+        console.log("Error en index.js -> setupApi(), el error es: ", error);
+    }
+}
+
+setupAPI()
+    .then(() => console.log("APi iniciada exitosamente"))
+    .catch((error) =>{
+        console.log("Error en index.js -> setupAPI(), el error es:", error);
+    });
